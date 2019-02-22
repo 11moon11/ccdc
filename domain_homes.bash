0a93e9ac46e61f2ce57949c6bfe91fff1322840b
Kirill <kirill23.1997@gmail.com>
2019-02-22 15:20:06 -0600

    Create home folders for domain users

---
+++ domain_homes.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+	echo "Please specify the domain (ex: CCDC)"
+	exit 0
+fi
+
+for u in $(wbinfo -u); do mkdir /home/$1/$u; chown $u:"Domain Users" /home/$1/$u; done
+
