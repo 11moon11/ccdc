47f16586c420f44836e1dd2057d62b065a149aa4
Kirill <kirill23.1997@gmail.com>
2019-02-22 20:00:31 -0600

    faillog

--- tool_kit.bash
+++ tool_kit.bash
@@ -8,6 +8,17 @@ showtty() {
     ps -aux | grep tty
 }
 
+showfaillog() {
+
+    if [ "`sudo ls /var/log/audit/audit.log 2>/dev/null`" != "" ]; then
+        sudo tail /var/log/audit/audit.log -n 40 | grep fail
+    fi
+
+    if [ "`sudo ls /var/log/auth.log 2>/dev/null`" != "" ]; then
+        sudo tail /var/log/auth.log -n 40 | grep fail
+    fi
+}
+
 if [ "`which chattr 2>/dev/null`" != "" ]; then
     sudo mv /usr/bin/chattr /usr/bin/ttr 
 fi
